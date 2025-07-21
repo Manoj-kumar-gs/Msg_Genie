7b509d140052f5adc0dcd04d0cd07ee14fc53183
Manoj-kumar-gs <manojappu315@gmail.com>
2025-07-22 03:37:39 +0530

    added metadata for MsgGenie

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,35 +1,3 @@
-// import { Skeleton } from "@/components/ui/skeleton"
-
-// export function SkeletonDemo() {
-//   return (
-//     <div className="flex items-center space-x-4 w-screen h-screen justify-center">
-//       <Skeleton className="h-[90%] w-[90%] rounded-lg" />
-//       <div className="space-y-2">
-//         <Skeleton className="h-4 w-[250px]" />
-//         <Skeleton className="h-4 w-[200px]" />
-//       </div>
-//     </div>
-//   )
-// }
-
-// import { cn } from "@/lib/utils"
-// import { Slider } from "@/components/ui/slider"
-
-// type SliderProps = React.ComponentProps<typeof Slider>
-
-// export function SkeletonDemo({ className, ...props }: SliderProps) {
-//   return (
-//     <Slider
-//       defaultValue={[50]}
-//       max={100}
-//       step={1}
-//       className={cn("w-[60%]", className)}
-//       {...props}
-//     />
-//   )
-// }
-
-
 import { Skeleton } from "@/components/ui/skeleton"
 
 export function SkeletonDemo() {
